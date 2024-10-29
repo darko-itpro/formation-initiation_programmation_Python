4b9753c4234a74f2ab3b37cec987a48582ad4694
Darko Stankovski <darko.itpro@gmaiL.com>
2024-10-29 11:09:26 +0100

    fonctions

--- exos/exo_03_02.py
+++ exos/exo_03_02.py
@@ -2,11 +2,17 @@
 training_to_cancel = ["Python, initiation", 2, 1200, 0]
 training_to_maintain = ["Python, les fondamentaux", 5, 2600, 4]
 
-training = training_to_maintain
-# training = training_to_cancel
+def is_maintained(training:list):
+    result = False
+    if training[3]:
+        result = True
 
-if training[3]:
-    print("Formation assurée")
-else:
-    print("Formation annulée")
+    return result
 
+
+def is_maintained(training: list) -> bool:
+    return bool(training[3])
+
+
+is_maintained(training_to_cancel)
+is_maintained(training_to_maintain)
