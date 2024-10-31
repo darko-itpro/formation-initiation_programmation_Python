f629f41b7291276151ba09bf7d03982ebd3b066c
Darko Stankovski <darko.itpro@gmaiL.com>
2024-10-31 12:26:04 +0100

    Correction dictionnaires

--- exos/exo_03_02.py
+++ exos/exo_03_02.py
@@ -2,11 +2,8 @@
 training_to_cancel = ["Python, initiation", 2, 1200, 0]
 training_to_maintain = ["Python, les fondamentaux", 5, 2600, 4]
 
-training = training_to_maintain
-training = training_to_cancel
+def is_maintained(training:list) -> bool:
+    return bool(training[3])
 
-if training[3]:
-    print('maintenu')
-else:
-    print('annulée')
-
+print(is_maintained(training_to_maintain))
+print(is_maintained(training_to_cancel))
--- exos/exo_06.py
+++ exos/exo_06.py
@@ -12,3 +12,29 @@ training_no_key = {"title": "Django, initiation",
                    "duration": 4,
                    "price": 2200,
                    }
+
+def is_maintained(training:dict) -> bool: # Pour vous, training a une clef "students"
+    if "students" not in training:
+        return False
+
+    return bool(training["students"])
+
+
+print(is_maintained(training_to_maintain))
+print(is_maintained(training_to_cancel))
+print(is_maintained(training_no_key))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
